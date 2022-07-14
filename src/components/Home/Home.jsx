be458f4dfa892306e4d54e6d8e60255c9550a61e
Ilmi <83828336+IrumiDesu7@users.noreply.github.com>
2022-07-14 16:06:06 +0800

    home page styling

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,47 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import Hero from '../Hero/Hero';
+import './home.css';
 
 export default function Home() {
+  const [ingredients, setIngredients] = useState([]);
+
+  const detailsElement = ingredients.map((ingredient) => {
+    return (
+      <Link
+        to={`ingredients/Chicken/${ingredient.idMeal}`}
+        className='detail-item'
+        key={`${ingredient.idMeal}`}
+      >
+        <div
+          style={{
+            backgroundImage: `url(${ingredient.strMealThumb})`,
+            backgroundSize: `cover`,
+          }}
+          className='food-image'
+        />
+        <h4 className='food-title'>{ingredient.strMeal}</h4>
+      </Link>
+    );
+  });
+
+  useEffect(() => {
+    async function getIngredientsDetails() {
+      const rest = await fetch(
+        `https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken`
+      );
+      const data = await rest.json();
+      setIngredients(data.meals);
+    }
+    getIngredientsDetails();
+  }, []);
   return (
-    <main>
+    <>
       <Hero />
-      <h1>Home page</h1>
-    </main>
+      <main className='home-container'>
+        <h2>Check out these recommended recipes for you</h2>
+        <div className='recommended-container'>{detailsElement}</div>
+      </main>
+    </>
   );
 }
